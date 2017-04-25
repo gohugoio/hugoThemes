4ca98a9298eaa158cd374029d8c8eb0d033b439a
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-04-25 21:37:16 +0200

    script: Add some more debug

--- buildThemeSite.sh
+++ buildThemeSite.sh
@@ -8,11 +8,19 @@ cd _script
 
 BASEURL=${BASEURL:-http://localhost:1313}
 
-./generateThemeSite.sh ${BASEURL} | exit 1
+./generateThemeSite.sh ${BASEURL}
+if [ $code -ne 0 ]; then
+	echo "generate theme site failed: exit status $code"
+	exit 1
+fi
 
 echo "Building site to public with baseURL ${BASEURL}..."
 
-hugo --quiet -s hugoThemeSite/themeSite -b ${BASEURL} | exit 1
+hugo -s hugoThemeSite/themeSite -b ${BASEURL}
+if [ $code -ne 0 ]; then
+	echo "building theme site failed: exit status $code"
+	exit 1
+fi
 
 cd ..
 
