4c5ff185fe94967e7bbe37f8b31119c0de5d4575
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-05-22 17:45:05 +0300

    Revert "experimental: Update submodules on build"
    This reverts commit 5aeb8500aaeaa55302dd79e572a4c9cb84364ffa.

--- buildThemeSite.sh
+++ buildThemeSite.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# We may move this update bit to CircleCI or Travis with Cron later, but for now we just trigger updates on every build.
-git submodule update --remote --merge
-if ! git diff-index --quiet HEAD --; then
-   git commit -am "Update themes"
-	git push
-fi
-
 echo "Hugo env:"
 
 hugo env
