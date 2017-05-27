6c70c61cc3373c0fea65b3632d46eb32ee267ef2
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2017-05-27 18:55:53 +0300

    Revert "Reintroduce: Update submodules on build"
    This reverts commit 7a8965d8ef43a36995602d63f77f5e8ea0e21c69.

--- buildThemeSite.sh
+++ buildThemeSite.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# We may move this update bit to CircleCI or Travis with Cron later, but for now we just trigger updates on every build.
-git config user.name "hugo-themebuilder"
-git config user.email "hugo-themebuilder@gohugo.io"
-git submodule update --remote --merge
-if ! git diff-index --quiet HEAD --; then
-   git commit -am "Update themes"
-   git remote add origin git@github.com:spf13/hugoThemes.git
-	git push origin master
-	if [ $? -ne 0 ]; then
-		exit 1
-	fi
-fi
-
 echo "Hugo env:"
 
 hugo env
