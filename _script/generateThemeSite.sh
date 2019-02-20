gohugoio/hugoThemes

commit 90afa18ac4ee39430c8f19a9d07a26d5c716c290

Add 2nd config for HugoBasicExample (#570)

_script/generateThemeSite.sh
@@ -59,6 +59,9 @@ configTplPrefix="config-tpl"
 configBase="${configTplPrefix}-base"
 configBaseParams="${configTplPrefix}-params"
 
+themeConfig="${TMPDIR}config-${x}.toml"
+taxoConfig="${siteDir}/exampleSite/configTaxo.toml"
+
 export HUGO_THEMESDIR=${themesDir}
 
 # This is the hugo Theme Site Builder
@@ -196,7 +199,7 @@ for x in `find ${themesDir} -mindepth 1 -maxdepth 1 -type d -not -path "*.git" -
             if [ "${inWhiteList}" != "" ]; then
             HUGO_THEME=${x} hugo --quiet -s exampleSite2 -d ${demoDestination} -b $BASEURL/theme/$x/
             else
-            HUGO_THEME=${x} hugo --quiet -s exampleSite2 -c ${siteDir}/exampleSite/content/ -d ${demoDestination} -b $BASEURL/theme/$x/
+            HUGO_THEME=${x} hugo --quiet -s exampleSite2 -c ${siteDir}/exampleSite/content/ --config=${themeConfig},${taxoConfig} -d ${demoDestination} -b $BASEURL/theme/$x/
             fi
             if [ $? -ne 0 ]; then
                 echo "FAILED to create exampleSite for $x"
@@ -209,7 +212,6 @@ for x in `find ${themesDir} -mindepth 1 -maxdepth 1 -type d -not -path "*.git" -
 
         else
 
-            themeConfig="${TMPDIR}config-${x}.toml"
             baseConfig="${configBase}.toml"
             paramsConfig="${configBaseParams}.toml"
 

commit a83765eeb7c5aee21fbbc79caa3f53d606553476

Build Script Fixes (#571)
This PR fixes bugs introduced with https://github.com/gohugoio/hugoThemes/pull/570

I have tested these fixes with a selection of themes and it fixes the issue reported over here: https://github.com/gohugoio/hugoThemes/issues/560#issuecomment-465625366

My apologies. I tested #570 with a different selection of themes and I missed the fact that the PATH I had defined for the first config (the one inherited from the exampleSite of each theme) was wrong.

cc: @digitalcraftsman

_script/generateThemeSite.sh
@@ -59,9 +59,6 @@ configTplPrefix="config-tpl"
 configBase="${configTplPrefix}-base"
 configBaseParams="${configTplPrefix}-params"
 
-themeConfig="${TMPDIR}config-${x}.toml"
-taxoConfig="${siteDir}/exampleSite/configTaxo.toml"
-
 export HUGO_THEMESDIR=${themesDir}
 
 # This is the hugo Theme Site Builder
@@ -178,6 +175,8 @@ for x in `find ${themesDir} -mindepth 1 -maxdepth 1 -type d -not -path "*.git" -
 	echo "source = \"$repo\"" >>themeSite/content/$x/index.md
 
 	demoDestination="../themeSite/static/theme/$x/"
+	demoConfig="${themesDir}/$x/exampleSite/config.toml"
+	taxoConfig="${siteDir}/exampleSite/configTaxo.toml"
 
 	export HUGO_CANONIFYURLS=true
 
@@ -199,7 +198,7 @@ for x in `find ${themesDir} -mindepth 1 -maxdepth 1 -type d -not -path "*.git" -
             if [ "${inWhiteList}" != "" ]; then
             HUGO_THEME=${x} hugo --quiet -s exampleSite2 -d ${demoDestination} -b $BASEURL/theme/$x/
             else
-            HUGO_THEME=${x} hugo --quiet -s exampleSite2 -c ${siteDir}/exampleSite/content/ --config=${themeConfig},${taxoConfig} -d ${demoDestination} -b $BASEURL/theme/$x/
+            HUGO_THEME=${x} hugo --quiet -s exampleSite2 -c ${siteDir}/exampleSite/content/ --config=${demoConfig},${taxoConfig} -d ${demoDestination} -b $BASEURL/theme/$x/
             fi
             if [ $? -ne 0 ]; then
                 echo "FAILED to create exampleSite for $x"
@@ -212,6 +211,7 @@ for x in `find ${themesDir} -mindepth 1 -maxdepth 1 -type d -not -path "*.git" -
 
         else
 
+            themeConfig="${TMPDIR}config-${x}.toml"
             baseConfig="${configBase}.toml"
             paramsConfig="${configBaseParams}.toml"
 