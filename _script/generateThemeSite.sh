7c5829e1a16ddc142f632e7d69b107749aa28aaf
Alex <onedrawingperday@gmail.com>
2019-02-23 23:09:42 +0200

    WIP

--- _script/generateThemeSite.sh
+++ _script/generateThemeSite.sh
@@ -131,7 +131,7 @@ noDemo=('hugo-incorporated', 'hugo-theme-arch', 'hugo-smpl-theme', 'lamp', 'hugo
 # yourfolio: portfolio theme needs its own content
 # hugo-resume: complicated resume theme needs its own content
 # hugo-mdl: author parameter is an inline table
-# hugo-dream-plus : author parameter is an inline table, also this theme needs itd 
+# hugo-dream-plus : author parameter is an inline table, also this theme needs its own content 
 whiteList=('academic', 'reveal-hugo', 'hugo-terrassa-theme', 'hugo-theme-learn', 'hugo-now-ui', 'dot-hugo-documentation-theme', 'cupper-hugo-theme', 'hugo-book', 'yourfolio', 'hugo-resume', 'hugo-mdl', 'hugo-dream-plus')
 
 errorCounter=0
