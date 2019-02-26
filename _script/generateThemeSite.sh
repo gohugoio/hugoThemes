8112539a031114f4a39b152b5588143ae010d767
Alex <onedrawingperday@gmail.com>
2019-02-27 00:09:33 +0200

    Fix layout WARN

--- _script/generateThemeSite.sh
+++ _script/generateThemeSite.sh
@@ -190,7 +190,7 @@ for x in `find ${themesDir} -mindepth 1 -maxdepth 1 -type d -not -path "*.git" -
 
     if $generateDemo; then
         if [ -d "${themesDir}/$x/exampleSite" ]; then
-cp -r ${siteDir}/exampleSite/content/i18n/ ${themesDir}/$x/exampleSitecontent/i18n/
+        cp -r -n ${siteDir}/layouts/_default/ ${themesDir}/$x/layouts/_default/
         	# Use content and config in exampleSite
             echo "Building site for theme ${x} using its own exampleSite to ${demoDestination}"
 
